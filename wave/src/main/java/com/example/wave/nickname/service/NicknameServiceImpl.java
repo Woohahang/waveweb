cb5572dbf9d69e65139bf88e8df37253fd61345c
hyeonWoo <glass1726@naver.com>
2024-09-25 03:37:37 +0900

    refactor: NicknameServiceImpl로 클래스 이름 변경

---
+++ wave/src/main/java/com/example/wave/nickname/service/NicknameServiceImpl.java
@@ -0,0 +1,74 @@
+package com.example.wave.nickname.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.example.wave.nickname.dto.NicknameDTO;
+import com.example.wave.nickname.entity.GameNickname;
+import com.example.wave.nickname.repository.NicknameRepository;
+import com.example.wave.user.entity.User;
+import com.example.wave.user.repository.UserRepository;
+
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2 // Log4j2 로깅 기능을 제공하는 어노테이션
+@Service // Spring의 서비스 컴포넌트로 등록
+@Validated // 유효성 검사를 활성화하는 어노테이션
+public class NicknameServiceImpl implements NicknameService {
+
+	@Autowired
+	private NicknameRepository nicknameRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	/**
+     * 사용자의 게임 닉네임을 저장하는 메서드입니다.
+     * @param dto 게임 닉네임 정보를 담고 있는 DTO
+     */
+	@Override
+	public void saveNickname(@Valid NicknameDTO dto) {
+		// userId로 DB에서 사용자 조회
+		User user = userRepository.findByUserId(dto.getUserDiscordId());
+		
+		// GameNickname 엔티티를 빌더 패턴을 사용하여 생성
+		GameNickname gameNickname = GameNickname.builder()
+				.user(user)
+	            .gameName(dto.getGameName())
+	            .nickname(dto.getNickname())
+	            .build();
+
+		// GameNickname 엔티티를 데이터베이스에 저장
+		nicknameRepository.save(gameNickname);
+	}
+	
+
+	/**
+     * 사용자 ID에 대한 게임 닉네임 목록을 조회하는 메서드입니다.
+     * @param userId 사용자 ID
+     * @return 해당 사용자의 게임 닉네임 목록
+     */
+	@Override
+	public List<GameNickname> getNicknames(String userId) {
+		// 사용자 ID로 GameNickname 목록을 조회
+		return nicknameRepository.findByUser_UserId(userId);
+	}
+
+
+	/**
+	 * 선택된 닉네임 ID 목록을 기반으로 게임 닉네임을 삭제하는 메서드입니다.
+	 * @param ids 삭제할 닉네임의 ID 목록
+	 */
+	@Override
+	public void deleteNickname(List<Long> ids) {
+		// 각 닉네임 ID에 대해 삭제 작업 수행
+		for (Long id : ids) {
+			nicknameRepository.deleteById(id); // 각 닉네임 ID로 삭제
+		}
+	}
+
+}
--- wave/src/main/java/com/example/wave/user/service/UserService.java
+++ wave/src/main/java/com/example/wave/user/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.wave.user.service;
 
 import com.example.wave.user.dto.UserDTO;
 
+import jakarta.validation.Valid;
+
 public interface UserService {
-	void saveOrUpdateUser(UserDTO userDTO);
-    void deleteUser(String userId);
+	void saveOrUpdateUser(@Valid UserDTO userDTO);
+    void deleteUser(@Valid String userId);
 }
