75db659ba1130b514a625bd959c9370353f53997
hyeonWoo <glass1726@naver.com>
2024-10-09 20:53:45 +0900

    refactor: 사용자 상태 조회 API를 OAuth2 인증에서 세션 기반으로 변경

--- wave/src/main/java/com/example/wave/user/controller/UserRestController.java
+++ wave/src/main/java/com/example/wave/user/controller/UserRestController.java
@@ -1,28 +1,36 @@
-//package com.example.wave.user.controller;
-//
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import com.example.wave.user.dto.UserDto;
-//import com.example.wave.user.mapper.UserMapper;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class UserRestController {
-//
-//	@GetMapping("/user")
-//	public Object getCurrentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
-//		if (oauth2User == null) {
-//			// 인증되지 않은 경우 홈 페이지로 리디렉션
-//			return new ModelAndView("redirect:/");
-//		}
-//
-//		UserDto userDto = UserMapper.toDto(oauth2User);
-//		return userDto; // OAuth2User에서 UserDto로 변환하여 반환
-//	}
-//
-//}
+package com.example.wave.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.wave.user.dto.UserDto;
+import com.example.wave.user.mapper.UserMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api")
+public class UserRestController {
+
+	@GetMapping("/user/status")
+	public ResponseEntity<UserDto> getUserStatus(HttpServletRequest request) {
+		System.out.println("getUserStatues 작동");
+		
+		
+	    UserDto userDto = (UserDto) request.getSession().getAttribute("userDto");
+	    if (userDto != null) {
+	    	System.out.println("반환 성공");
+	        return ResponseEntity.ok(userDto); // UserDto 반환
+	    } else {
+	    	System.out.println("반환 실패");
+	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 인증되지 않은 경우
+	    }
+	}
+
+}
